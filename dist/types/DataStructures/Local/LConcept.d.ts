f2d153bdde774d0d8da5320af8ef65eb604f8402
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-05-21 15:55:57 +0545

    changes

--- dist/types/DataStructures/Local/LConcept.d.ts
+++ dist/types/DataStructures/Local/LConcept.d.ts
@@ -12,6 +12,7 @@ export declare class LConcept {
     updatedTimeStamp: Date;
     type: LConcept | null | void | Concept;
     isNew: boolean;
+    isComposition: boolean;
     isTemp: boolean;
     constructor(id: number, userId: number, typeId: number, categoryId: number, accessId: number, characterValue: string, typeCharacter: string, isNew: boolean | undefined, entryTimeStamp: Date, updatedTimeStamp: Date);
     getType(): void;
