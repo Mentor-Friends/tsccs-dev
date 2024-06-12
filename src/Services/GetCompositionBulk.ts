cedb433434581a535372949c3c14a5b319a2bc04
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-06-12 10:01:02 +0545

    searching multiple

--- src/Services/GetCompositionBulk.ts
+++ src/Services/GetCompositionBulk.ts
@@ -1,7 +1,8 @@
 import { GetAllConnectionsOfCompositionBulk } from "../Api/GetAllConnectionsOfCompositionBulk";
 import { GetConnectionBulk } from "../Api/GetConnectionBulk";
 import { Connection } from "../DataStructures/Connection";
-import { CheckForConnectionDeletion, CheckForConnectionDeletionWithIds } from "./CheckForConnectionDeletion";
+import { ConnectionData } from "../app";
+import { CheckForConnectionDeletionWithIds } from "./CheckForConnectionDeletion";
 import { FindConnectionsOfCompositionsBulkInMemory } from "./FindConnectionsOfCompositionBulkInMemory";
 import { GetCompositionFromMemory, GetCompositionWithIdFromMemory } from "./GetComposition";
 
@@ -35,6 +36,24 @@ export async function GetCompositionFromConnectionsWithDataId(ids:number[]=[], c
     for(let i=0; i< ids.length;i++){
        var comp = await GetCompositionWithIdFromMemory(ids[i]);
         compositions.push(comp);
+    }
+    return compositions;
+}
+
+
+export async function GetCompositionFromConnectionsWithDataIdInObject(ids:number[]=[], connections:number[] = []){
+    let remainingConnections: number[] = [];
+    for(let i=0; i< connections.length; i++){
+        let connection = await ConnectionData.GetConnection(connections[i]);
+        if(connection.id == 0){
+            remainingConnections.push(connections[i]);
+        }
+    }
+    await GetConnectionBulk(remainingConnections);
+    var compositions:any = {};
+    for(let i=0; i< ids.length;i++){
+       var comp = await GetCompositionWithIdFromMemory(ids[i]);
+        compositions[ids[i]] = comp;
     }
     return compositions;
 }
