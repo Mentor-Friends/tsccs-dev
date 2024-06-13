2b7a16495bd2c8f954ddf0e82b94eac7bd665bfa
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2024-06-13 17:35:10 +0545

    added changes to connection local

--- src/Services/Local/CreateTheConnectionLocal.ts
+++ src/Services/Local/CreateTheConnectionLocal.ts
@@ -7,13 +7,22 @@ export  function CreateTheConnectionLocal(ofTheConceptId:number, toTheConceptId:
      typeId: number,orderId:number = 1,
     ){  
         var accessId : number = 4;
+        let randomid = Math.floor(Math.random() * 100000000);
+        let realOfTheConceptId = 0;
+        let realToTheConceptId = 0;
+        let realTypeId = 0;
+        realOfTheConceptId = ofTheConceptId;
+        realToTheConceptId = toTheConceptId;
+        realTypeId = typeId;
+        let connection = new LConnection(0,0,0,0,0,0);
         if(ofTheConceptId != toTheConceptId){
-            var connection = new LConnection(0, ofTheConceptId, toTheConceptId, typeId, orderId, accessId);
+             connection = new LConnection(0, ofTheConceptId, toTheConceptId, typeId, orderId, accessId);
             connection.isTemp = true;
             connection.id = Math.floor(Math.random() * 100000000);
             LocalConnectionData.AddConnection(connection);
             storeToDatabase("localconnection", connection);
-            return connection;
         }
+        return connection;
+
       
 }
