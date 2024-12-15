b19242e67f769d86f164fff228993cd6dfef0556
biprash-MentorFriends <biprashgautam@mentorfriends.com>
2024-12-15 15:47:16 +0545

    fix: message queue

--- src/app.ts
+++ src/app.ts
@@ -416,7 +416,7 @@ export async function sendMessage(type: string, payload: any) {
   return new Promise((resolve, reject) => {
     // navigator.serviceWorker.ready
     //   .then((registration) => {
-    if (navigator.serviceWorker.controller) {
+    if ((navigator.serviceWorker.controller || serviceWorker) && (serviceWorkerReady || type == 'init')) {
       const responseHandler = (event: any) => {
         if (event?.data?.messageId == messageId) { // Check if the message ID matches
           if (!event.data.success) {
