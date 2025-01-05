366fdebe8a69d0059f581a2f8219495c3b29fec4
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2025-01-05 15:42:15 +0545

    added changes to widget tree adding origin

--- src/Widgets/WidgetTree.ts
+++ src/Widgets/WidgetTree.ts
@@ -16,6 +16,8 @@ export class WidgetTree{
     after_render: string = "";
     before_render: string = "";
     update: string = "";
+    origin: number = 0;
+    version: number = 0;
     mount_child: string = "";
     children:WidgetTree[] = [];
     wrapper: number = 0;
