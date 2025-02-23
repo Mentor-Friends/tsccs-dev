d5fa1ef18ae0a5a3a699abfac88724c73de9a64a
biprash-MentorFriends <biprashgautam@mentorfriends.com>
2025-02-23 17:51:05 +0545

    fix: multiple fetch

--- src/Api/MakeTheTypeConceptApi.ts
+++ src/Api/MakeTheTypeConceptApi.ts
@@ -6,52 +6,48 @@ import { ConceptsData, GetConceptByCharacter } from "../app";
 import { GetConceptByCharacterAndCategory } from "../Services/ConceptFinding/GetConceptByCharacterAndCategory";
 import { HandleHttpError } from "../Services/Common/ErrorPosting";
 
+const conceptCache = new Map<string, Promise<Concept>>();
 
 // This function is used to check the type concpet of a passed string
 // if the text is "the_person" then the function finds the related concept
 export async function MakeTheTypeConceptApi(type:string, userId:number){
-  let startTime = new Date().getTime();
   // create  a default concept with all defaulting to zero
-    let concept = CreateDefaultConcept();
+  let concept = CreateDefaultConcept();
+
+  // check the cache
+  if (conceptCache.has(type)) return conceptCache.get(type) || concept;
+
+  const makeTypeConcept = (async () => {
     try{
-
-       // get the concept by character and category from the api
-        concept = await GetConceptByCharacterAndCategory(type);
-        if(concept.id == 0 || concept.typeId == 4){
-          
-          console.log("this is the concept that needs to be found", type, concept.typeId);
-            var header = GetRequestHeader('application/x-www-form-urlencoded');
-            const response = await fetch(BaseUrl.MakeTheTypeConceptUrl(),{
-              method: 'POST',
-              headers: header,
-              body: `type=${type}`
-            });
-            if(!response.ok){
-                HandleHttpError(response);
-                throw new Error(`Error! status: ${response.status}`);
-            }
-            let result = await response.json();
-            concept = result as Concept;
-            if(concept.id > 0){
-              ConceptsData.AddConcept(concept);
-            }
-
+      // get the concept by character and category from the api
+      concept = await GetConceptByCharacterAndCategory(type);
+      if(concept.id == 0 || concept.typeId == 4){
+        var header = GetRequestHeader('application/x-www-form-urlencoded');
+        const response = await fetch(BaseUrl.MakeTheTypeConceptUrl(),{
+          method: 'POST',
+          headers: header,
+          body: `type=${type}`
+        });
+        if(!response.ok){
+          HandleHttpError(response);
+          throw new Error(`Error! status: ${response.status}`);
         }
-        var end = new Date().getTime();
-        var time = end - startTime;
-        console.log('Execution time: ' + time + "This is the type" + type + "----" + concept.typeId);
-        return concept;
-
-
-
-
+        let result = await response.json();
+        concept = result as Concept;
+        if(concept.id > 0) ConceptsData.AddConcept(concept);
       }
-      catch (error) {
-        if (error instanceof Error) {
-          console.log('Make The Type Concept Api error : ', error.message);
-        } else {
-          console.log('Make The Type Concept Api error : ', error);
-        }
-        throw error;
-      }
+      return concept;
+    }
+    catch (error) {
+      if (error instanceof Error) console.log('Make The Type Concept Api error : ', error.message);
+      else console.log('Make The Type Concept Api error : ', error);
+      throw error;
+    }
+    finally {
+      conceptCache.delete(type); // Remove from cache after fetching
+    }
+  })()
+  // set the cache when fetching
+  conceptCache.set(type, makeTypeConcept);
+  return makeTypeConcept;
 }
