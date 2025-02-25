86c0fc03345a3d8a4327cd9e6ffeef89f23afbef
prakash-mentorfriends <prakash.lama29@boomconsole.com>
2025-02-25 16:09:18 +0545

    fixed types and added check for element node

--- src/Services/Search/JustIdFormat.ts
+++ src/Services/Search/JustIdFormat.ts
@@ -279,7 +279,7 @@ export async function FormatFromConnectionsAlteredArrayExternalJustId(connection
                       isComp = true;
                     }
                     if(linkerConceptValue == ""){
-                      linkerConceptValue = toTheConcept?.type?.characterValue;
+                      linkerConceptValue = toTheConcept?.type?.characterValue || "";
                     }
                     try{
                         let mytype = toTheConcept?.type?.characterValue ?? "none";
