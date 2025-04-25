84829ada0af92865a373ab9b03643de1ac9b3196
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2025-04-25 12:40:47 +0545

    added composition

--- src/prototype/prototype.service.ts
+++ src/prototype/prototype.service.ts
@@ -6,27 +6,48 @@ import CreateTheConceptLocal from "../Services/Local/CreateTheConceptLocal";
 export async function createPrototypeLocal(prototype:Prototype){
 
   let allConcepts:Concept[] = [];
-  let mainPrototype:Concept = await addPrototype(prototype.prototype, prototype.isCompositional);
+  let allConnections: Connection[] = [];
+  let requiredConnections: Connection[] = [];
+  let mainPrototype:Concept = CreateDefaultConcept();
+  let optionalConnections:Connection[] = [];
+  if(prototype.isCompositional){
+    mainPrototype = await addCompositionPrototype(prototype.prototype, allConcepts,allConnections);
+
+  }
+  else{
+    mainPrototype = await addPrototype(prototype.prototype);
+    requiredConnections = await AddPrototypeConnections(prototype.required, mainPrototype, allConcepts, true);
+    optionalConnections = await AddPrototypeConnections(prototype.optional, mainPrototype, allConcepts);
+    allConcepts.push(mainPrototype);
+
+  }
 
 
-  let requiredConnections = await AddPrototypeConnections(prototype.required, mainPrototype, allConcepts, true);
-  let optionalConnections = await AddPrototypeConnections(prototype.optional, mainPrototype, allConcepts);
-  allConcepts.push(mainPrototype);
+
+  let connectionTypeString = typeSemantic(mainPrototype, true, true);
+  let connectionType:Concept = await MakeTheTypeConceptLocal(connectionTypeString, 999,999,999);
+
+  allConcepts.push(connectionType);
 
   if(prototype.childPrototypes.length > 0){
     for(let i=0; i< prototype.childPrototypes.length; i++){
         let ChildPrototypeWrapper = await createPrototypeLocal(prototype.childPrototypes[i]);
         let childPrototype:Concept = ChildPrototypeWrapper["mainConcept"];
-        let connectionTypeString = typeSemantic(mainPrototype, true, true)
-        let connectionType:Concept = await MakeTheTypeConceptLocal(connectionTypeString, 999,999,999);
-        await CreateTheConnectionLocal(mainPrototype.id, childPrototype.id, connectionType.id, 1000, connectionTypeString,999);
+        let childConnection = await CreateTheConnectionLocal(mainPrototype.id, childPrototype.id, connectionType.id, 1000, connectionTypeString,999);
+        requiredConnections.push(childConnection);
+        allConcepts.push(childPrototype);
     }
   }
   
   requiredConnections = [...requiredConnections, ...optionalConnections];
+  for(let i=0; i<allConnections.length; i++){
+    requiredConnections.push(allConnections[i]);
+  }
 
   prototype.concepts = [...allConcepts, ...prototype.addedConcepts];
   prototype.connections = [...requiredConnections, ...prototype.addedConnections];
+  console.log("this is the prototype before", requiredConnections);
+  console.log("this is the prototype after", prototype);
   return{
     "concepts": allConcepts,
     "connections": requiredConnections,
@@ -36,22 +57,24 @@ export async function createPrototypeLocal(prototype:Prototype){
 }
 
 
-export async function addPrototype(type:string, isComposition:boolean = false){
-    let concept  = CreateDefaultConcept();
-    if(isComposition){
+export async function addPrototype(type:string){
+        let prototypeOf = await MakeTheTypeConceptLocal("the_prototype_of", 999, 999, 999);
+        let prototypeOfConcept = await MakeTheTypeConceptLocal(type, 999,999,999);
+        let concept  = await CreatePrototypeConcept(prototypeOf, prototypeOfConcept, 999);
+        return concept;
+}
+
+export async function addCompositionPrototype(type:string, concepts:Concept[] , connections:Connection[]){
+        let concept  = CreateDefaultConcept();
         let mainPrototype = await MakeTheInstanceConceptLocal("the_composition", "", true, 999, 999, 999);
         let compositionalPrototypeConnection = await MakeTheTypeConceptLocal("the_composition_type", 999, 999, 999);
         let prototypeConcept = await MakeTheTypeConceptLocal(type, 999, 999, 999);
-        await CreateTheConnectionLocal(mainPrototype.id, prototypeConcept.id, compositionalPrototypeConnection.id, 1000, "the_composition_type");
+        let typeCompositionConnection = await CreateTheConnectionLocal(mainPrototype.id, prototypeConcept.id, compositionalPrototypeConnection.id, 1000, "the_composition_type");
         concept = mainPrototype;
-    }
-    else{
-        let prototypeOf = await MakeTheTypeConceptLocal("the_prototype_of", 999, 999, 999);
-        let prototypeOfConcept = await MakeTheTypeConceptLocal(type, 999,999,999);
-        concept  = await CreatePrototypeConcept(prototypeOf, prototypeOfConcept, 999);
-    }
-
-    return concept;
+        concepts.push(compositionalPrototypeConnection);
+        concepts.push(prototypeConcept);
+        connections.push(typeCompositionConnection);
+        return concept;
 }
 
 export async function CreatePrototypeConcept(prototypeConcept:Concept, typeConcept:Concept, userId:number){
@@ -74,7 +97,7 @@ export function typeSemantic(mainPrototype:Concept, required:boolean = true, isC
         connectionType = connectionType + "_optional";
     }
     if(isComposition){
-        connectionType = connectionType + "the_composition";
+        connectionType = connectionType + "_the_composition";
     }
     return connectionType;
 
