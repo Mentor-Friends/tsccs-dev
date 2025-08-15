0b9531726e7904ad99c5924acb7023624cd9770d
Anil Maharjan <anilmaharjan@mentorfriends.com>
2025-08-15 12:30:21 +0545

    add: normalize css for nestings

---
+++ src/Widgets/NormalizeStyles.service.ts
@@ -0,0 +1,16 @@
+import postcss from "postcss";
+import nested from "postcss-nested";
+
+// Function to process CSS string
+export async function normalizeCSS(cssString: string) {
+  try {
+    const result = await postcss([nested()]).process(cssString, {
+      from: undefined,
+    });
+    console.log("result --->", result.css);
+    return result.css;
+  } catch (error) {
+    console.error("Error processing CSS:", error);
+    return null;
+  }
+}
